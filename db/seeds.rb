56050abbb3c9698e3bd264a052fe507c3cc6e813
Oliver Switzer <oliverswitzer@gmail.com>
2014-01-14 18:51:58 -0500

    added creator to test_trail

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,16 @@
 
 #NOT CURRENTLY WORKING
 
-test_user = User.create(username: "switzerswish",
-                        first_name: "Oliver",
-                        last_name: "Switzer",
-                        email: "oliverswitzer@gmail.com",
-                        password: "oliver123",
-                        password_confirmation: "oliver123")
+# test_user = User.create(username: "switzerswish",
+#                         first_name: "Oliver",
+#                         last_name: "Switzer",
+#                         email: "oliverswitzer@gmail.com",
+#                         password: "oliver123",
+#                         password_confirmation: "oliver123")
 
-test_trail = Trail.create(length: 10, popularity: 4, description: "OOOH SO PERTY TRAIL IS THE NATUREZ", name: "Nature Trail")
+# test_trail = Trail.create(length: 10, popularity: 4, description: "OOOH SO PERTY TRAIL IS THE NATUREZ", name: "Nature Trail")
+
+test_trail = Trail.find_by_name("Nature Trail")
+test_user = User.find_by_username("switzerswish")
+test_trail.creator = test_user
+test_trail.save
