75b6f48326be7d5f9a5efda1833d2462f35228db
Oliver Switzer <oliverswitzer@gmail.com>
2014-01-14 14:01:38 -0500

    add validation to the user for email and username (presence, uniqueness, length for username)

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,12 @@ class User < ActiveRecord::Base
   has_many :followed_trails,
     source: :trail,
     through: :trail_followers
+
+  before_save { self.email = email.downcase }  
+  before_save { self.username = username.downcase }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :username, presence: true, length: {maximum: 30}, uniqueness: true
+  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}
 end
