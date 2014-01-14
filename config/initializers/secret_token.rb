142d362b03a184f8601e7fb7b9435ebc4f76a50e
Nikki Thean <nikki.thean@gmail.com>
2014-01-14 10:21:34 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HappyFuntimeTrail::Application.config.secret_key_base = 'f2940d3fe7a1db8b773e099f812fcb2b790578e8833c37ec198deb6e5ae72b6ef0ddb8a6500184f142dc128f95ca79b0d8685f257793720255765ca9bc44354f'
