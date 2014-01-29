5768efbb69e90e911f33184d4bce8d04c21fe80a
Nikki Thean <nikki.thean@gmail.com>
2014-01-29 14:18:13 -0500

    Add name to pins

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140122174323) do
+ActiveRecord::Schema.define(version: 20140129184342) do
 
   create_table "addresses", force: true do |t|
     t.string  "line1"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20140122174323) do
     t.integer "trail_id"
     t.decimal "latitude"
     t.decimal "longitude"
+    t.string  "name"
   end
 
   create_table "tags", force: true do |t|
