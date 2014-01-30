8690b9e5e853aec8dc836a6be3515eaeb48bc321
Nikki Thean <nikki.thean@gmail.com>
2014-01-30 15:12:28 -0500

    Attempt header stuff

--- app/assets/javascripts/sitewide/header.js
+++ app/assets/javascripts/sitewide/header.js
@@ -4,19 +4,23 @@ $(document).ready(ready);
 $(document).on('page:load', ready);
 
 function ready() {
-  var click_count = 0;
   var $compass = $(".compass img");
   var $header = $(".header");
 
   setTimeout(function(){
-    $header.animate(
-      {
-        top: "-60px"
-      },
-      "normal");  
-  }, 2000);  
-  
+    $header.animate({ top: "-60px" }, "normal");  
+  }, 2000);
+
+  $compass.toggle()
     
+  function pullUp() {
+    $header.animate({ top: "-60px"}, "normal");
+  }
+
+  function pullDown() {
+    $header.animate({ top: "0px"}, "normal");
+  }
+
   $compass.click(function (){
     if(click_count % 2 == 0) {
       console.log("Open sesame");
