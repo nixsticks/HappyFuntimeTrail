249cac8b471266c99f0f0d101262c9e4b9d4df35
Nikki Thean <nikki.thean@gmail.com>
2014-01-30 15:16:08 -0500

    Readd click count

--- app/assets/javascripts/sitewide/header.js
+++ app/assets/javascripts/sitewide/header.js
@@ -1,16 +1,17 @@
 // header.js
 
 $(document).ready(ready);
-$(document).on('page:load', ready);
+$(document).on('page:change', ready);
 
 function ready() {
+  var click_count = 0;
   var $compass = $(".compass img");
   var $header = $(".header");
 
   setTimeout(function(){
     $header.animate({ top: "-60px" }, "normal");  
   }, 2000);
-  
+
   $compass.click(function (){
     if(click_count % 2 == 0) {
       console.log("Open sesame");
