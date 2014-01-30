870de66954ba74803a0a5e09e4ab3bb717555fd2
Nikki Thean <nikki.thean@gmail.com>
2014-01-30 15:50:51 -0500

    Update header

--- app/assets/javascripts/sitewide/header.js
+++ app/assets/javascripts/sitewide/header.js
@@ -1,33 +1,16 @@
 // header.js
 
-$(document).ready(ready);
 $(document).on('page:change', ready);
 
 function ready() {
-  var click_count = 0;
   var $compass = $(".compass img");
-  var $header = $(".header");
+  var $header = $("nav");
 
   setTimeout(function(){
-    $header.animate({ top: "-60px" }, "normal");  
+    $header.removeClass("down");
   }, 2000);
 
-  $compass.click(function (){
-    if(click_count % 2 == 0) {
-      console.log("Open sesame");
-      $header.animate(
-      {
-        top: "0px"
-      }, 
-      "normal");
-    } else {
-      console.log("Close sesame");
-      $header.animate(
-      {
-        top: "-60px"
-      },
-      "normal");    
-    }
-    click_count++
+  $compass.on("click", function(event) {
+    $header.toggleClass("down");
   });
 }
